67428a40dff7695c10e819cd0a2b8d0806618b0b
jr17061991leo <jr17061991leo@gmail.com>
2019-09-24 00:04:08 -0300

    implementação da classe employe

--- src/entidades/Employe.java
+++ src/entidades/Employe.java
@@ -1,5 +1,43 @@
 package entidades;
 
 public class Employe {
+	private Integer id;
+	private String nome;
+	private double salario;
+
+	public Employe(Integer id, String nome, double salario) {
+
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public void aumentoSalaraio (double percentual) {
+		this.salario += (salario * (percentual/100));
+	}
 
 }
--- src/funcionariosList/Programa.java
+++ src/funcionariosList/Programa.java
@@ -3,7 +3,7 @@ package funcionariosList;
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
